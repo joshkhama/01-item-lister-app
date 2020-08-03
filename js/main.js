33259888d97ef22786316aa5f9d1fe4588abf29d
joshua khama <kowhegbajosh@gmail.com>
2020-08-03 10:38:29 +0100

    added the delete button and search input

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 var form = document.getElementById("addForm");
 var itemList = document.getElementById("items");
+var filter = document.getElementById("search");
 
 // Form submit event
 form.addEventListener("submit", addItem);
+// Delete event
+itemList.addEventListener("click", removeItem);
+// Filter event
+filter.addEventListener("keyup", filterItems);
 
 // Add item
 function addItem(e) {
@@ -23,4 +28,31 @@ function addItem(e) {
   li.appendChild(delBtn);
   // Add the li into the list
   itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e) {
+  if (e.target.classList.contains("btn")) {
+    if (confirm("Are You Sure?")) {
+      let li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+// Filter Items
+function filterItems(e) {
+  //convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get list
+  var items = itemList.getElementsByTagName("li");
+  // Convert to an array
+  Array.from(items).forEach(function (item) {
+    var itemName = item.firstChild.textContent;
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
